3005dbd7691dafcb8b2c98003887bad891912c5b
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-09-14 11:13:31 -0400

    An autocomplete field should only show if the `autocomplete` flag is set in the function invocation's options hash, or when in the admin more than 100 users are returned for the query.
    Introduce wp.select2, which stores query arguments per dropdown field
    instance, which is passed to the user query AJAX call.
    
    Fix some documentation, use boolean defaults rather than `0` or `1`.
    
    First run at l10n.
    
    Allow numerous instances of an autocomplete dropdown.

--- includes/js/wp-select2.js
+++ includes/js/wp-select2.js
@@ -1,44 +1,57 @@
 (function($) {
-	$('#post_author_override').select2({
-		// @todo: override default CSS of .select2-container instead.
-		width: '100%',
-		initSelection: function(element, callback) {
-			var data = { id: element.val(), text: element.data('selected-show') };
-			callback(data);
-		},
-		/**
-		 * Use a custom WordPress endpoint for querying users.
-		 */
-		ajax: {
-			url: wp.ajax.settings.url,
-			dataType: 'json',
-			/**
-			 * Create the settings hash for the $.ajax call.
-			 *
-			 * @param string search Search input by user.
-			 * @param int    page   Pagination of request.
-			 * @return object
-			 */
-			data: function (search, page) {
-				return {
-					action: 'get_users',
-					search: search,
-					page_limit: 10,
-					page: page
-				};
+	/**
+	 * Turn any element with the `.wp-users-dropdown` class into a Select2 dropdown.
+	 */
+	$('.wp-users-dropdown').each( function( index, element ) {
+		var $el = $(element),
+			name = $el.attr('name'),
+			queryArgs = wp.select2[name].queryArgs,
+			show = wp.select2[name].show;
+		// Need to know how many users to query per page for infinite scroll.
+		queryArgs.number = queryArgs.number || 10;
+		$el.select2({
+			// @todo: override default CSS of .select2-container instead.
+			width: '100%',
+			initSelection: function(element, callback) {
+				var data = { id: element.val(), text: element.data('selected-show') };
+				callback(data);
 			},
 			/**
-			 * Parse results from AJAX request into format expected by Select2.
-			 *
-			 * @param  object data Response.
-			 * @param  int    page
-			 * @return objet
+			 * Use a custom WordPress endpoint for querying users.
 			 */
-			results: function (data, page) {
-				var more = (page * 10) < data.data.total;
+			ajax: {
+				url: wp.ajax.settings.url,
+				dataType: 'json',
+				/**
+				 * Create the settings hash for the $.ajax call.
+				 *
+				 * @param string search Search input by user.
+				 * @param int    page   Pagination of request.
+				 * @return object
+				 */
+				data: function (search, page) {
+					return {
+						action: 'get_users',
+						search: search,
+						query_args: queryArgs,
+						show: show,
+						page_limit: queryArgs.number,
+						page: page
+					};
+				},
+				/**
+				 * Parse results from AJAX request into format expected by Select2.
+				 *
+				 * @param  object data Response.
+				 * @param  int    page
+				 * @return objet
+				 */
+				results: function (data, page) {
+					var more = (page * queryArgs.number) < data.data.total;
 
-				return { results: data.data.users, more: more };
+					return { results: data.data.users, more: more };
+				}
 			}
-		}
+		});
 	});
 })(jQuery);
